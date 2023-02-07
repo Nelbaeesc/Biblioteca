5acc79ed303de0f3e312cd24ac03ee1aab110dc0
Nelson Baez <nelbaeesc@alu.edu.gva.es>
2023-02-07 19:27:45 +0100

    Pruebas

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,7 +1,11 @@
 public class Biblioteca {
+
     private ListaSE<Libro> libros;
+
     private ListaSE<Revista> revista;
+
     private ListaSE<Periodico> periodico;
+
     private ListaSE<Ejemplar> ejemplar;
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,14 +13,14 @@ public class Main {
 
         libros.addHead(new Libro(Color.COLOR,"Goma",29,"Ivan","La vida de los If's","454545",libros));
         libros.addHead(new Libro(Color.BLANCONEGRO,"Verde",20,"Jose","La vida de los While's","4545845",libros));
-//        revistas.addHead(new Revista(Color.BLANCONEGRO,"Coc",8,"Un dia un for","Diario",Periocidad.SEMANAL,new Date()));
+        revistas.addHead(new Revista(Color.BLANCONEGRO,"Coc",8,"Un dia un for","Diario",Periocidad.SEMANAL,new Date()));
 //        periodicos.addHead(new Periodico(Color.COLOR,"Whilers",12,"Marca",new Date()));
 
 //        System.out.println(libros);
 
        // libro.devolucion(new Libro(Color.COLOR,"Goma",29,"Ivan","La vida de los If's","454545",libros),1);
 
-        System.out.println(libros);
+        System.out.println(revistas);
 
     }
 }
